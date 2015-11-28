ec5ce33ca3233ef58b56d840e8b2c8e0c14f3c0f
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2015-11-28 12:04:08 +0000

    WWAudioFilter 1.0.16

--- PlayPcmWin/WWAudioFilter/OverlappedFft.cs
+++ PlayPcmWin/WWAudioFilter/OverlappedFft.cs
@@ -13,12 +13,17 @@ namespace WWAudioFilter {
         private bool mFirstTime;
         private double[] mWindow;
 
+        private double[] mLastOutputSamplesTail;
+
+        private int mSpliceDenominator = 32;
+        private int mOffset = 0;
+
         public OverlappedFft(int fftLength) {
             mFftLength = fftLength;
             mFft = new WWRadix2Fft(mFftLength);
             mOverlapInputSamples = new double[mFftLength / 2];
             mWindow = WWWindowFunc.BlackmanWindow(mFftLength / 2 - 1);
-
+            mLastOutputSamplesTail = new double[mFftLength / mSpliceDenominator];
             mFirstTime = true;
         }
 
@@ -69,10 +74,32 @@ namespace WWAudioFilter {
 
             var timeComplex = mFft.InverseFft(freqDomain);
 
+#if false
+            for (int i = 0; i < mFftLength; ++i) {
+                System.Console.WriteLine("{0},{1}", i + mOffset, timeComplex[i].real);
+            }
+#endif
+            mOffset += mFftLength / 2;
+
             // 逆FFT結果の時間ドメインの真ん中データを戻す。
             var result = new double[mFftLength / 2];
             for (int i = 0; i < mFftLength / 2; ++i) {
                 result[i] = timeComplex[i + mFftLength / 4].real;
+            }
+
+            if (!mFirstTime) {
+                // 出力結果の最初の部分を、最後の出力結果の対応部分とミックスする。
+                // ブチっという音を抑制するため。
+                for (int i = 0; i < mFftLength / mSpliceDenominator; ++i) {
+                    double secondGain = (double)i / ( mFftLength / mSpliceDenominator);
+                    double firstGain = 1.0 - secondGain;
+                    result[i] = firstGain * mLastOutputSamplesTail[i] + secondGain * result[i];
+                }
+            }
+
+            // 出力結果最後の先の部分を次回処理で使用するため保存する。
+            for (int i = 0; i < mFftLength / mSpliceDenominator; ++i) {
+                mLastOutputSamplesTail[i] = timeComplex[i + mFftLength *3 / 4].real;
             }
 
             mFirstTime = false;
