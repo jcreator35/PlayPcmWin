a997bafe315ec139979924fcc0de1c5be3ea68be
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2016-02-11 09:37:24 +0000

    PlayPcmWin 4.0.98

--- PlayPcmWin/WasapiCS/Properties/AssemblyInfo.cs
+++ PlayPcmWin/WasapiCS/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System;
 
 [assembly: Guid("3d0c480e-3543-4bd3-be96-d34ed55fb464")]
 
-[assembly: AssemblyVersion("2.0.99.0")]
-[assembly: AssemblyFileVersion("2.0.99.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
