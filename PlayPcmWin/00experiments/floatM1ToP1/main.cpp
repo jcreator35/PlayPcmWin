1379acc84d590e94db9dd1e0ebecdb91db2436ad
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2016-05-19 13:35:32 +0000

    FloatM1ToP1

--- PlayPcmWin/00experiments/floatM1ToP1/main.cpp
+++ PlayPcmWin/00experiments/floatM1ToP1/main.cpp
@@ -3,9 +3,12 @@
 #include <xmmintrin.h> //< _MM_SET_FLUSH_ZERO_MODE
 #include <set>
 
-static const float RECIP_32768 = 1.0f / 32768.0f;
-static const float RECIP_8388608 = 1.0f / 8388608.0f;
-static const float RECIP_2147483648 = 1.0f / 2147483648.0f;
+static const int S16_SUP = 32768;
+static const int S24_SUP = 8388608;
+static const int64_t S32_SUP = 2147483648LL;
+static const float RECIP_S16SUP = 1.0f / S16_SUP;
+static const float RECIP_S24SUP = 1.0f / S24_SUP;
+static const float RECIP_S32SUP = 1.0f / S32_SUP;
 
 // subnormal number flush to zero
 static void
@@ -18,15 +21,15 @@ static bool
 Int16ToFloatToInt16(void)
 {
     std::set<float> floatSet;
-    for (int v=-32768; v<32768; ++v) {
-        float f = v * RECIP_32768;
+	for (int v = -S16_SUP; v<S16_SUP; ++v) {
+		float f = v * RECIP_S16SUP;
         floatSet.insert(f);
     }
     printf("Int16 to Float: %lld elements\n", (int64_t)floatSet.size());
 
     std::set<int> intSet;
     for (auto ite = floatSet.begin(); ite != floatSet.end(); ++ite) {
-        int v = (int)(32768.0f * (*ite));
+		int v = (int)(S16_SUP * (*ite));
         intSet.insert(v);
     }
     printf("Int16 to Float to Int16: %lld elements\n", (int64_t)intSet.size());
@@ -38,15 +41,15 @@ static bool
 Int24ToFloatToInt24(void)
 {
     std::set<float> floatSet;
-    for (int v=-8388608; v<8388608; ++v) {
-        float f = v * RECIP_8388608;
+	for (int v = -S24_SUP; v<S24_SUP; ++v) {
+        float f = v * RECIP_S24SUP;
         floatSet.insert(f);
     }
     printf("Int24 to Float: %lld elements\n", (int64_t)floatSet.size());
 
     std::set<int> intSet;
     for (auto ite = floatSet.begin(); ite != floatSet.end(); ++ite) {
-        int v = (int)(8388608.0f * (*ite));
+		int v = (int)(S24_SUP * (*ite));
         intSet.insert(v);
     }
     printf("Int24 to Float to Int24: %lld elements\n", (int64_t)intSet.size());
@@ -58,15 +61,15 @@ static bool
 Int32ToFloatToInt32(void)
 {
     std::set<float> floatSet;
-    for (int64_t v=-2147483648; v<2147483648; ++v) {
-        float f = v * RECIP_2147483648;
+	for (int64_t v = -S32_SUP; v<S32_SUP; ++v) {
+		float f = v * RECIP_S32SUP;
         floatSet.insert(f);
     }
     printf("Int32 to Float: %lld elements\n", (int64_t)floatSet.size());
 
     std::set<int> intSet;
     for (auto ite = floatSet.begin(); ite != floatSet.end(); ++ite) {
-        int v = (int)(2147483648 * (*ite));
+		int v = (int)(S32_SUP * (*ite));
         intSet.insert(v);
     }
     printf("Int32 to Float to Int32: %lld elements\n", (int64_t)intSet.size());
@@ -74,6 +77,7 @@ Int32ToFloatToInt32(void)
     return true;
 }
 
+#if 0
 static bool
 FloatCount(void)
 {
@@ -108,14 +112,26 @@ FloatCount(void)
 
     return true;
 }
+#endif
 
 int main(void)
 {
-    // EnableFtz();
-    //FloatCount();
+#if 0
+	// experiment 1
+	FloatCount();
+#else
+	// experiment 2
     Int16ToFloatToInt16();
     Int24ToFloatToInt24();
     Int32ToFloatToInt32();
 
-    return 0;
+	EnableFtz();
+	printf("FTZ enabled. subnormal flushes to zero.\n");
+
+	Int16ToFloatToInt16();
+	Int24ToFloatToInt24();
+	Int32ToFloatToInt32();
+#endif
+
+	return 0;
 }
