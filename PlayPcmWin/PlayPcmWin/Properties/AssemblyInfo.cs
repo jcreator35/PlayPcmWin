2821576f9fee280227c58d5fef4280fe15c3204f
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2017-01-22 15:07:18 +0000

    PlayPcmWin 5.0.23 WWOfflineResampler 1.0.7

--- PlayPcmWin/PlayPcmWin/Properties/AssemblyInfo.cs
+++ PlayPcmWin/PlayPcmWin/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.0.22.0")]
-[assembly: AssemblyFileVersion("5.0.22.0")]
+[assembly: AssemblyVersion("5.0.23.0")]
+[assembly: AssemblyFileVersion("5.0.23.0")]
