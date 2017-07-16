93b152916c5545e8d6c8b7f5d8d0b3e7c275ffd3
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2017-07-16 04:32:14 +0000

    Maximum Length Sequence Cross-correlation

---
+++ PlayPcmWin/WWMath/CrossCorrelation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WWMath {
+    class CrossCorrelation {
+
+        /// <summary>
+        /// Cross-correlation.
+        /// when a==b, result is autocorrelation.
+        /// 
+        /// https://en.wikipedia.org/wiki/Cross-correlation
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double[] CalcCrossCorrelation(double[] a, double[] b) {
+            var c = new double[b.Length];
+
+            for (int n = 0; n < b.Length; ++n) {
+                c[n] = 0;
+                for (int m = 0; m < a.Length; ++m) {
+                    if (b.Length <= m + n) {
+                        break;
+                    }
+                    c[n] += a[m] * b[m + n];
+                }
+            }
+
+            return c;
+        }
+
+        /// <summary>
+        /// Circular Cross-Correlation.
+        /// when a==b, result is circular autocorrelation.
+        /// </summary>
+        public static double[] CalcCircularCrossCorrelation(double[] a, double[] b) {
+            var c = new double[b.Length];
+
+#if true
+            Parallel.For(0, b.Length, n => {
+#else
+            for (int n = 0; n < b.Length; ++n) {
+#endif
+                c[n] = 0;
+                for (int m = 0; m < a.Length; ++m) {
+                    int bPos = (m + n) % b.Length;
+                    c[n] += a[m] * b[bPos];
+                }
+            });
+
+            return c;
+        }
+    }
+}
---
+++ PlayPcmWin/WWMath/MaximumLengthSequence.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace WWMath {
+    class MaximumLengthSequence {
+        /// <summary>
+        /// Maximum Length Sequenceを生成する。
+        /// 値は1か0、(2^order)-1要素の配列が出る。
+        /// order==4のとき、(2^4)-1 = 15個のsequenceが出る
+        /// 
+        /// https://en.wikipedia.org/wiki/Maximum_length_sequence
+        /// </summary>
+        public static byte[] Create(int order) {
+            System.Diagnostics.Debug.Assert(4 <= order);
+            System.Diagnostics.Debug.Assert(order <= 30);
+
+            int count = (int)(Math.Pow(2, order) - 1);
+            var b = new byte[count];
+            var a = new byte[2][];
+            a[0] = new byte[order];
+            a[1] = new byte[order];
+
+            a[0][0] = 1;
+
+            int cur = 0;
+            int next = 1;
+            for (int pos = 0; pos < count; ++pos) {
+                a[next][order - 1] = (byte)((a[cur][0] + a[cur][1]) % 2);
+                for (int i = order - 2; 0 <= i; --i) {
+                    a[next][i] = a[cur][i + 1];
+                }
+
+                b[pos] = a[next][0];
+
+                cur = cur == 0 ? 1 : 0;
+                next = next == 0 ? 1 : 0;
+            }
+
+            return b;
+        }
+    }
+}
