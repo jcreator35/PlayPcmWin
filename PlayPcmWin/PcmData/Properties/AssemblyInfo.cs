b539244322759d51c7b4c6174a178b69845fb967
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2017-08-11 15:55:55 +0000

    PlayPcmWin 5.0.42

--- PlayPcmWin/PcmData/Properties/AssemblyInfo.cs
+++ PlayPcmWin/PcmData/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System;
 
 [assembly: Guid("8a8890e1-7aea-4d9a-810e-5f5a09131cce")]
 
-[assembly: AssemblyVersion("2.0.88.9")]
-[assembly: AssemblyFileVersion("2.0.88.9")]
+[assembly: AssemblyVersion("2.0.88.10")]
+[assembly: AssemblyFileVersion("2.0.88.10")]
