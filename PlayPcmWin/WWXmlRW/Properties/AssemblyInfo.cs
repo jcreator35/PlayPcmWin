d3c2825c13b6574b679c82b0ab080dd0fc23fb20
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2017-08-20 13:05:51 +0000

    PlayPcmWin 5.0.44

--- PlayPcmWin/WWXmlRW/Properties/AssemblyInfo.cs
+++ PlayPcmWin/WWXmlRW/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System;
 // 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
 [assembly: Guid("ede8f428-b971-47b6-bcf7-4b42c23c13f6")]
 
-[assembly: AssemblyVersion("1.0.18.11")]
-[assembly: AssemblyFileVersion("1.0.18.11")]
+[assembly: AssemblyVersion("1.0.18.12")]
+[assembly: AssemblyFileVersion("1.0.18.12")]
