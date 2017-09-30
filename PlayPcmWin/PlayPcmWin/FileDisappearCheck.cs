98bb8c1a8653009c5270e854005a599f4b33195b
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2017-09-30 12:43:30 +0000

    FileDisapeearCheck to check music file disappear event, typically happens when SD card eject

---
+++ PlayPcmWin/PlayPcmWin/FileDisappearCheck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace PlayPcmWin {
+    class FileDisappearCheck {
+        public delegate void FileDisapeearedEventHandler(string path);
+
+        public FileDisappearCheck(string path, FileDisapeearedEventHandler ev) {
+            mPath = path;
+            mFileDisappeared = ev;
+
+            mFsw = new FileSystemWatcher(Path.GetDirectoryName(path), Path.GetFileName(path));
+            mFsw.Deleted += new FileSystemEventHandler(OnDeleted);
+            mFsw.Renamed += new RenamedEventHandler(OnRenamed);
+            mFsw.Error += new ErrorEventHandler(OnError);
+        }
+
+        void OnError(object sender, ErrorEventArgs e) {
+            Console.WriteLine("FileSystemWatcher raised Error event");
+
+            if (e.GetException().GetType() == typeof(InternalBufferOverflowException)) {
+                Console.WriteLine(("FileSystemWatcher InternalBufferOverflowException " + e.GetException().Message));
+            }
+        }
+
+        void OnRenamed(object sender, RenamedEventArgs e) {
+            mFileDisappeared(mPath);
+        }
+
+        void OnDeleted(object sender, FileSystemEventArgs e) {
+            mFileDisappeared(mPath);
+        }
+
+        private string mPath;
+        private FileSystemWatcher mFsw;
+        private FileDisapeearedEventHandler mFileDisappeared;
+    }
+}
