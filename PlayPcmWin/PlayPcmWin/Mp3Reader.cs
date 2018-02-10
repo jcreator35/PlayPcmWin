bd73d062b71029759ac1b8b4e7eab32ad4d9148b
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2018-02-10 02:58:23 +0000

    forgot to add Mp3Reader.cs

---
+++ PlayPcmWin/PlayPcmWin/Mp3Reader.cs
@@ -0,0 +1,31 @@
+﻿// 日本語UTF-8
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using WWMFReaderCs;
+using System.Linq;
+
+namespace PlayPcmWin {
+
+    class Mp3Reader {
+        public string path;
+        public byte[] data;
+        public WWMFReader.Metadata meta;
+
+        public int Read(string path) {
+            this.path = path;
+
+            int hr = WWMFReader.ReadHeaderAndData(path, out meta, out data);
+            if (hr < 0) {
+                return hr;
+            }
+
+            return 0;
+        }
+
+        public void ReadStreamEnd() {
+            data = null;
+        }
+    };
+}
