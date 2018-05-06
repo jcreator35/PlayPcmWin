173732a16bc8e6dbf53c679839a69e7e6861257d
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2018-05-06 06:38:53 +0000

    PlayPcmWin 5.0.66 PPWRemote6v2

--- PlayPcmWin/WWAudioFilter/Properties/AssemblyInfo.cs
+++ PlayPcmWin/WWAudioFilter/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.45.30")]
-[assembly: AssemblyFileVersion("1.0.45.30")]
+[assembly: AssemblyVersion("1.0.45.31")]
+[assembly: AssemblyFileVersion("1.0.45.31")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
