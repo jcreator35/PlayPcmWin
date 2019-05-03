1144643b8525ceff0cf7458ab62e667c4c36094f
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2019-05-03 05:46:13 +0000

    add WWVectorD2

---
+++ PlayPcmWin/WWMath/WWVectorD2.cs
@@ -0,0 +1,26 @@
+﻿
+namespace WWMath {
+    public class WWVectorD2 {
+
+        private double[] v = new double[2];
+        public WWVectorD2() {
+        }
+
+        public WWVectorD2(double x, double y) {
+            v[0] = x;
+            v[1] = y;
+        }
+
+        public double X {
+            get {
+                return v[0];
+            }
+        }
+
+        public double Y {
+            get {
+                return v[1];
+            }
+        }
+    }
+}
