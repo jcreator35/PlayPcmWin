1aaee40ee6f3e55fa065e0b23719ea94403b65f7
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2019-09-10 12:39:51 +0000

    WWAudioFilter 1.0.50

--- PlayPcmWin/WavRWLib2/Properties/AssemblyInfo.cs
+++ PlayPcmWin/WavRWLib2/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System;
 
 [assembly: Guid("89fef4a1-c300-4efd-a35d-517b2002a522")]
 
-[assembly: AssemblyVersion("3.0.10.0")]
-[assembly: AssemblyFileVersion("3.0.10.0")]
+[assembly: AssemblyVersion("3.0.11.0")]
+[assembly: AssemblyFileVersion("3.0.11.0")]
