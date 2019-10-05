7095dc895e81a5bd1d6c4ff9964535283eadd30c
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2019-10-05 12:05:54 +0000

    Stable Goertzel algorithm

--- PlayPcmWin/WWMath/WWGoertzel.cs
+++ PlayPcmWin/WWMath/WWGoertzel.cs
@@ -5,7 +5,7 @@ using WWUtil;
 
 namespace WWMath {
     /// <summary>
-    ///Stable Goertzel algorithm
+    /// Stable Goertzel algorithm
     /// Richard G. Lyons, Understanding Digital Signal Processing, 3 rd Ed., Pearson, 2011, pp. 840
     /// </summary>
     public class WWGoertzel {
@@ -27,14 +27,14 @@ namespace WWMath {
             mDelay = new DelayT<WWComplex>(1);
             mDelay.Fill(WWComplex.Zero());
 
-            mOsc = new WWQuadratureOscillatorInt(m, N);
+            mOsc = new WWQuadratureOscillatorInt(-m, N);
         }
 
         /// <summary>
-        /// 時間ドメイン値xを入力すると、N点DFTの周波数ドメイン
+        /// 時間ドメイン値xを1サンプル入力、N点DFTの周波数ドメイン値Xのm番目の周波数成分値を出力。
         /// </summary>
-        /// <param name="x"></param>
-        /// <returns></returns>
+        /// <param name="x">時間ドメイン値x</param>
+        /// <returns>X^m(q)</returns>
         public WWComplex Filter(double x) {
             var c = mOsc.Next();
             var m = WWComplex.Mul(c, x);
