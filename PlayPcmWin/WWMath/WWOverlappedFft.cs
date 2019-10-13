a06090dde3d813eb2d86c41f91f075f186e6ecfd
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2019-10-13 00:11:30 +0000

    Overlapped FFT moved to WWMath

--- PlayPcmWin/WWMath/WWOverlappedFft.cs
+++ PlayPcmWin/WWMath/WWOverlappedFft.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using WWMath;
-
-namespace WWAudioFilterCore {
+﻿
+namespace WWMath {
     public class WWOverlappedFft {
         WWTimeDependentForwardFourierTransform mFFTfwd;
         WWTimeDependentInverseFourierTransform mFFTinv;
 
+        /// <summary>
+        /// 入力データが完全に復元できる特殊な窓関数を使用したオーバーラップ・アドFFT。
+        /// あらかじめSetNumSamples()を呼ぶと入出力サンプル数が一致する。
+        /// </summary>
         public WWOverlappedFft(int fftLength) {
             mFFTfwd = new WWTimeDependentForwardFourierTransform(fftLength, WWTimeDependentForwardFourierTransform.WindowType.Hann);
             mFFTinv = new WWTimeDependentInverseFourierTransform(fftLength);
