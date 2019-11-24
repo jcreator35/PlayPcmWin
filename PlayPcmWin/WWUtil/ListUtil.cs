f3dd9afce0d18c9cdfa033a12907be410cab9cbe
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2019-11-24 12:03:50 +0000

    WWUtil added ListUtil

---
+++ PlayPcmWin/WWUtil/ListUtil.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WWUtil {
+    public class ListUtil {
+        /// <summary>
+        /// fromのstartPosからcount個のデータを取り出し配列として戻す。
+        /// もしもcount個無いときは、戻るデータの個数は減る。
+        /// 一つもないときは要素数0個の配列が戻る。
+        /// </summary>
+        public static T[] GetArrayFragment<T>(List<T[]> from, long startPos, int count) {
+            if (startPos < 0) {
+                throw new ArgumentOutOfRangeException("startPos");
+            }
+            if (count < 0) {
+                throw new ArgumentOutOfRangeException("count");
+            }
+
+            var r = new T[count];
+
+            long accPos = 0;
+            int toPos = 0;
+            foreach (var item in from) {
+                // 例1:
+                // startPos = 10
+                // bytes = 10
+                // itemLen=8のとき、
+                // 1個目のitemは accPos=0, itemLen=8  → スキップ。
+                // 2個目のitemは accPos=8, itemLen=8  → fromPos=2, toPos = 0, copyCount=6
+                // 3個目のitemは accPos=16, itemLen=8 → fromPos=0, toPos = 6, copyCount=4
+
+                if (accPos + item.Length <= startPos) {
+                    // まだ最初の取得位置よりも前。
+                    accPos += item.Length;
+                    continue;
+                }
+
+                // ちょうど取得データが有る場所に来た。
+                long fromPos = startPos - accPos;
+                int copyCount = item.Length;
+                if (startPos + count < fromPos + item.Length) {
+                    copyCount = (int)((startPos + count) - fromPos);
+                }
+                if (0 < copyCount) {
+                    Array.Copy(item, fromPos, r, toPos, copyCount);
+                    toPos += copyCount;
+                } else {
+                    // 最後のコピーデータよりも後の位置に来た。
+                    break;
+                }
+            }
+
+            if (toPos < count) {
+                // toPos個しか取得できなかったので戻り配列rのサイズを変更する。
+                var tmp = new T[toPos];
+                Array.Copy(r, 0, tmp, 0, toPos);
+                r = tmp;
+            }
+
+            return r;
+        }
+
+        public static LargeArray<T> GetLargeArrayFragment<T>(List<T[]> from, long startPos, long count) {
+            if (startPos < 0) {
+                throw new ArgumentOutOfRangeException("startPos");
+            }
+            if (count < 0) {
+                throw new ArgumentOutOfRangeException("count");
+            }
+
+            var r = new LargeArray<T>(count);
+
+            long accPos = 0;
+            long toPos = 0;
+            foreach (var item in from) {
+                // 例1:
+                // startPos = 10
+                // bytes = 10
+                // itemLen=8のとき、
+                // 1個目のitemは accPos=0, itemLen=8  → スキップ。
+                // 2個目のitemは accPos=8, itemLen=8  → fromPos=2, toPos = 0, copyCount=6
+                // 3個目のitemは accPos=16, itemLen=8 → fromPos=0, toPos = 6, copyCount=4
+
+                if (accPos + item.Length <= startPos) {
+                    // まだ最初の取得位置よりも前。
+                    accPos += item.Length;
+                    continue;
+                }
+
+                // ちょうど取得データが有る場所に来た。
+                int fromPos = (int)(startPos - accPos);
+                int copyCount = item.Length;
+                if (startPos + count < fromPos + item.Length) {
+                    copyCount = (int)((startPos + count) - fromPos);
+                }
+                if (0 < copyCount) {
+                    r.CopyFrom(item, fromPos, toPos, copyCount);
+                    toPos += copyCount;
+                } else {
+                    // 最後のコピーデータよりも後の位置に来た。
+                    break;
+                }
+            }
+
+            if (toPos < count) {
+                // toPos個しか取得できなかったので戻り配列rのサイズを変更する。
+                var tmp = new LargeArray<T>(toPos);
+                tmp.CopyFrom(r, 0, 0, toPos);
+                r = tmp;
+            }
+
+            return r;
+        }
+    }
+}
