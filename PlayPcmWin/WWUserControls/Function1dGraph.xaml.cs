1db7a0e0d76657f9d16a7813bb4ba6b32df80b86
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2020-03-14 14:21:05 +0000

    Function1d Graph User control

---
+++ PlayPcmWin/WWUserControls/Function1dGraph.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WWUserControls {
+    public partial class Function1dGraph : UserControl {
+        public Function1dGraph() {
+            InitializeComponent();
+        }
+
+        public string Title {
+            get { return (string)labelTitle.Content; }
+            set { labelTitle.Content = value; }
+        }
+
+        public string YAxis {
+            get { return (string)labelY.Content; }
+            set { labelY.Content = value; }
+        }
+
+        public string XAxis {
+            get { return (string)labelX.Content; }
+            set { labelX.Content = value; }
+        }
+    }
+}
