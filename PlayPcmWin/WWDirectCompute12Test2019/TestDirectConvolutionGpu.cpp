2c2ca33c4cd74a4ecd98527113e22f523f2c4642
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2020-07-18 20:08:07 +0000

    GPU Convolution test code updated

--- PlayPcmWin/WWDirectCompute12Test2019/TestDirectConvolutionGpu.cpp
+++ PlayPcmWin/WWDirectCompute12Test2019/TestDirectConvolutionGpu.cpp
@@ -12,23 +12,37 @@ TestDirectConvolutionGpu(void)
     WWDirectConvolutionGpu dc;
 
     const int INPUT_COUNT = 10;
-    const int CONV_COUNT = 5; // {1, 2, 3, 4, 5}
+    const int CONV_COUNT  = 5; 
 
-    float inputAry[INPUT_COUNT];
-    double convCoeffsAry[CONV_COUNT];
-    float outAry[INPUT_COUNT];
-
+    float inputAry[INPUT_COUNT]; // {1,1,1,1,1, 1,1,1,1,1}
     for (int i = 0; i < INPUT_COUNT; ++i) {
         inputAry[i] = 1.0f;
     }
+
+    double convCoeffsAry[CONV_COUNT]; // {1,2,3,4,5}
     for (int i = 0; i < CONV_COUNT; ++i) {
         convCoeffsAry[i] = i + 1;
     }
 
+    float outAry[INPUT_COUNT]; // Conv result, should be {6,10,15,15,15, 15,15,15,14,12}
+
     HRG(dc.Setup(inputAry, INPUT_COUNT, convCoeffsAry, CONV_COUNT));
 
+#if 1
+    {   // Calc all
+        ZeroMemory(outAry, sizeof outAry);
+
+        HRG(dc.Dispatch(0, INPUT_COUNT));
+        HRG(dc.ResultGetFromGpuMemory(outAry, INPUT_COUNT));
+
+        for (int i = 0; i < INPUT_COUNT; ++i) {
+            printf("%d : %f\n", i, outAry[i]);
+        }
+}
+#endif
+
 #if 0
-    {
+    {   // Calc the first half
         ZeroMemory(outAry, sizeof outAry);
 
         HRG(dc.Dispatch(0, INPUT_COUNT / 2));
@@ -40,8 +54,8 @@ TestDirectConvolutionGpu(void)
     }
 #endif
 
-#if 1
-    {
+#if 0
+    {   // Calc the latter half
         ZeroMemory(outAry, sizeof outAry);
 
         HRG(dc.Dispatch(INPUT_COUNT / 2, INPUT_COUNT));
