b52af5cfda7f8398029d979c0db17852c0304562
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2020-11-07 10:04:51 +0000

    Polynomial fit test

---
+++ PlayPcmWin/WWMathTest/WWPolynomialFitTest.cs
@@ -0,0 +1,25 @@
+﻿// 日本語。
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WWMath;
+
+namespace WWMathTest {
+    [TestClass]
+    public class WWPolynomialFitTest {
+        [TestMethod]
+        public void PolynomialFitTest1() {
+            var x = new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            var y = new double[] {
+                0, -0.37,-1.29,-2.43,-3.54,
+                -4.53,-5.38,-6.09,-6.69,-7.19,
+                -7.6,-7.95,-8.24,-8.49,-8.71,
+                -8.89,-9.04,-9.18,-9.3,-9.4,
+                -9.49,
+            };
+
+            var r = WWPolynomialFit.Calc(y, x, 6);
+
+            Assert.AreEqual(r.c.Length, 7);
+        }
+    }
+}
