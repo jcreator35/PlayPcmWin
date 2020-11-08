b95ce2ce5afa5983a1ba56865af73a06d1728bce
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2020-11-08 06:23:49 +0000

    Polynomial fit test

--- PlayPcmWin/WWMathTest/WWPolynomialFitTest.cs
+++ PlayPcmWin/WWMathTest/WWPolynomialFitTest.cs
@@ -17,14 +17,27 @@ namespace WWMathTest {
                 -9.49,
             };
 
+#if true
+            var r = WWPolynomialFit.Calc(y_dB, x, 6);
+
+            /*
+             Result:
+             
+             y_dB = -0.0000029212346025337816 x^6
+                 +0.00020291497408909238 x^5
+                 -0.0054888099286801205 x^4
+                 +0.071110615465301924 x^3
+                 -0.40078216359333169 x^2
+                 -0.11354738870338571 x
+                 +0.028327961846712043
+            */
+#else
             var y = new double[y_dB.Length];
             for (int i = 0; i < y.Length; ++i) {
                 y[i] = Math.Pow(10, y_dB[i] / 20.0);
             }
 
             var r = WWPolynomialFit.Calc(y, x, 6);
-
-            Assert.AreEqual(r.c.Length, 7);
 
             /*
              Result:
@@ -37,6 +50,8 @@ namespace WWMathTest {
                  -0.031365202488051289 x
                  +1.0053057661304232
             */
+#endif
+            Assert.AreEqual(r.c.Length, 7);
         }
     }
 }
